db59f3f720331299e9c61751ac3bb1f6deed950d
Subhom <baruahsubhom@gmail.com>
2026-01-29 22:25:34 +0530

    Can quit now

--- test.py
+++ test.py
@@ -3,3 +3,14 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((1920,1920))
+running = True
+
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+pygame.quit()
+
+
+
