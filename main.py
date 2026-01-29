f598f6df36fc2e068539323e478348837ff441eb
EternusCor <muhammedshahinjaleel@gmail.com>
2026-01-29 23:42:52 +0530

    grid class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from grid import grid
 
 pygame.init()
 
@@ -7,29 +8,11 @@ running = True
 disp_x = 600
 disp_y = 600
 
-
-
 screen = pygame.display.set_mode((disp_x, disp_y))
 
+new_grid = grid(screen, disp_x, disp_y)
 
-grid_line_size = 1
-grid_color = (255, 255, 255)
-grid_size = 25
-
-def draw_grid():
-  
-  for x in range(disp_x):
-    if x%grid_size == 0:
-      pygame.draw.line(screen, grid_color, (x, 0), (x, disp_y), grid_line_size)
-  
-  for y in range(disp_y):
-    if y%grid_size == 0:
-      pygame.draw.line(screen, grid_color, (0, y), (disp_x, y), grid_line_size)
-
-
-
-draw_grid()
-
+new_grid.draw_grid()
 
 while running:
   for event in pygame.event.get():
